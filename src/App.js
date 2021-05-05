da2358b5ace585bb2d2136cbfe4d15be1365f6ff
toddmotto <todd@toddmotto.com>
2021-05-05 14:56:41 +0100

    02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <Router>Hello</Router>
+      <Router>
+        <Route path="/" element={<div>Root</div>} />
+      </Router>
     </div>
   );
 };
