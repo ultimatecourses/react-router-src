8f02a3fdc212a7176800f40107d59b26b6a9f220
toddmotto <todd@toddmotto.com>
2021-05-06 12:07:11 +0100

    05

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { css } from '@emotion/css';
 
 import Products from './Products/Products';
@@ -24,7 +29,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Products />} />
             <Route path="/admin" element={<Admin />} />
-            <Route path="*" element={<div>Not Found</div>} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </div>
       </Router>
