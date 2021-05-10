32adbba4575ed4ba31ae065f0885488aa7a2a04c
toddmotto <todd@toddmotto.com>
2021-05-10 15:20:25 +0100

    14

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { css } from '@emotion/css';
+
+import { retrieveProduct } from './ProductsService';
+
+const ProductStyles = css`
+  color: #fff;
+  background: #2a2c37;
+  border-radius: 6px;
+  padding: 15px;
+  .Product {
+    &-Title {
+      display: flex;
+    }
+    &-Name {
+      font-weight: 600;
+      font-size: 1.2rem;
+      margin: 0;
+    }
+    &-Price {
+      color: #50fa7b;
+      font-weight: 600;
+      font-size: 1rem;
+      margin: 0;
+    }
+    &-Icon {
+      width: 50px;
+      margin-right: 15px;
+    }
+  }
+`;
 
 const Product = () => {
   const { id } = useParams();
-  console.log(id);
-  return <div>Product</div>;
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const product = await retrieveProduct(id);
+      setProduct(product);
+    })();
+  }, [id]);
+
+  if (product === null) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={ProductStyles}>
+      <div className="Product-Title">
+        <img
+          src={`/assets/img/products/${product.id}.svg`}
+          alt={product.name}
+          className="Product-Icon"
+        />
+        <div>
+          <h1 className="Product-Name">{product.name}</h1>
+          <p className="Product-Price">{`$${product.price / 100}`}</p>
+        </div>
+      </div>
+      <div className="Product-Description">
+        <p>{product.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/Products/ProductsService.js
+++ src/Products/ProductsService.js
@@ -5,3 +5,11 @@ export const listProducts = async () => {
   }
   throw new Error('Something went wrong');
 };
+
+export const retrieveProduct = async (id) => {
+  const response = await fetch(`/api/products/${id}`);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Error('Something went wrong');
+};
