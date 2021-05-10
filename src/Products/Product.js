e8e6afa3a3c74b9c0a894d3d375ce0fc5af679da
toddmotto <todd@toddmotto.com>
2021-05-10 14:30:20 +0100

    13

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
+  const { id } = useParams();
+  console.log(id);
   return <div>Product</div>;
 };
 
