af2b9567eba27d17755a91758212fda990a40e8b
toddmotto <todd@toddmotto.com>
2021-05-18 13:27:09 +0100

    24

--- src/Products/ProductEdit.js
+++ src/Products/ProductEdit.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { css } from '@emotion/css';
 
 import { createProduct } from './ProductsService';
@@ -8,9 +9,40 @@ const ProductEditStyles = css`
   background: #2a2c37;
   border-radius: 6px;
   padding: 15px;
+  .ProductEdit {
+    &-Input {
+      width: 100%;
+      border: 1px solid transparent;
+      color: #fff;
+      background: #1d1e26;
+      padding: 10px 15px;
+      margin-bottom: 5px;
+      border-radius: 6px;
+      outline: 0;
+      &:focus {
+        border-color: #50fa7b;
+      }
+    }
+    &-Textarea {
+      min-height: 80px;
+      resize: none;
+    }
+    &-Button {
+      border: 2px solid #50fa7b;
+      color: #50fa7b;
+      background: none;
+      padding: 10px 15px;
+      border-radius: 6px;
+      outline: 0;
+      cursor: pointer;
+      font-weight: 600;
+      text-transform: uppercase;
+    }
+  }
 `;
 
 const ProductEdit = () => {
+  const navigate = useNavigate();
   const [form, setForm] = useState(null);
 
   useEffect(() => {
@@ -31,8 +63,8 @@ const ProductEdit = () => {
 
   const handleCreate = async () => {
     try {
-      const created = await createProduct(form);
-      console.log(created);
+      const { id } = await createProduct(form);
+      navigate(`/admin/${id}`);
     } catch (e) {
       console.warn(e);
     }
