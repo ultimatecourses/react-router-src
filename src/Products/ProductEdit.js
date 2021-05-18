c82712ee8f424ef23d7d74e6647b37c2ce66fe77
toddmotto <todd@toddmotto.com>
2021-05-18 12:38:27 +0100

    23

--- src/Products/ProductEdit.js
+++ src/Products/ProductEdit.js
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { css } from '@emotion/css';
+
+import { createProduct } from './ProductsService';
+
+const ProductEditStyles = css`
+  color: #fff;
+  background: #2a2c37;
+  border-radius: 6px;
+  padding: 15px;
+`;
 
 const ProductEdit = () => {
-  return <div>I am the product!</div>;
+  const [form, setForm] = useState(null);
+
+  useEffect(() => {
+    setForm({
+      id: '',
+      name: '',
+      price: 0,
+      description: '',
+    });
+  }, []);
+
+  const updateField = ({ name, value }) => {
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleCreate = async () => {
+    try {
+      const created = await createProduct(form);
+      console.log(created);
+    } catch (e) {
+      console.warn(e);
+    }
+  };
+
+  if (form === null) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <form className={ProductEditStyles}>
+      <input
+        type="text"
+        name="id"
+        placeholder="ID"
+        className="ProductEdit-Input"
+        value={form.id}
+        onChange={({ target }) => updateField(target)}
+      />
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        className="ProductEdit-Input"
+        value={form.name}
+        onChange={({ target }) => updateField(target)}
+      />
+      <input
+        type="text"
+        name="price"
+        placeholder="Price"
+        className="ProductEdit-Input"
+        value={form.price}
+        onChange={({ target }) =>
+          updateField({ name: target.name, value: parseInt(target.value, 10) })
+        }
+      />
+      <textarea
+        name="description"
+        placeholder="Description"
+        className="ProductEdit-Input ProductEdit-Textarea"
+        value={form.description}
+        onChange={({ target }) => updateField(target)}
+      />
+      <button
+        type="button"
+        className="ProductEdit-Button"
+        onClick={handleCreate}
+      >
+        Create
+      </button>
+    </form>
+  );
 };
 
 export default ProductEdit;
--- src/Products/ProductsService.js
+++ src/Products/ProductsService.js
@@ -6,6 +6,21 @@ export const listProducts = async () => {
   throw new Error('Something went wrong');
 };
 
+export const createProduct = async (payload) => {
+  const response = await fetch(`/api/products`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Error('Something went wrong');
+};
+
 export const retrieveProduct = async (id) => {
   const response = await fetch(`/api/products/${id}`);
   if (response.ok) {
